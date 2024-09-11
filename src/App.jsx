ac0aefd17e66113417554041eaa5061a063f8413
Koohyewon <khw6746@naver.com>
2024-09-11 21:07:27 +0900

    App.js 라우트 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,36 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import MovieDetail from "./pages/MovieDetail";
 
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/movie-detail", element: <MovieDetail /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <SignUp /> },
+];
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // 토큰 확인 로직
-    const token = localStorage.getItem("token"); // 또는 다른 방식으로 토큰을 저장/확인
-    setIsLoggedIn(!!token);
+    //토큰 확인 전, 후 헤더 구분
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token); //이중 부정 연산자
   }, []);
 
   return (
+    /* 최소 크기를 100vh로 설정 후, flex-grow 사용해서 contents 부분이 Footer를 제외한 나머지 부분을 차지하게 해서 항상 Footer가 가장 아래에 bottom-0 처럼 위치하도록 함 */
     <div className="flex flex-col min-h-screen">
       <BrowserRouter>
         <div className="flex-grow">
           {isLoggedIn ? <HeaderAfterLogin /> : <HeaderBeforeLogin />}
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/movie-detail" element={<MovieDetail />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
           </Routes>
         </div>
       </BrowserRouter>
